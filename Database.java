841af793a500152a7c31e865d7f7ac7c7a560c9d
huynh-ha-mai-trinh <hhmtrinh@apcs.vn>
2018-07-31 23:19:22 +0700

    Search Task (search, suggest, search result act..)

---
+++ Database.java
@@ -0,0 +1,35 @@
+/*
+    This class control all data of the application
+ */
+
+package com.example.hhmt.findfunction;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.util.Pair;
+
+public class Database {
+
+    private static List<Products> product_data; //store product database
+
+    //Product database methods
+    Products getProductById(int id){
+        return product_data.get(id);
+    }
+
+    public void addProduct(Products product){
+        if (product_data==null)
+            product_data = new ArrayList<Products>();
+
+        //Add a product
+        product_data.add(product);
+
+        //Update Database tree
+        TrieTree tree = new TrieTree();
+        tree.updateTreeViaDatabase(new Pair<String,Integer>(product.getName()+" "+product.getDescription(),product_data.size()-1));
+    }
+
+    //User database methods
+    //
+    //
+}
